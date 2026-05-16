4a7aa46507a16b6df5787ece64c9dd37e843b1f6
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-05-16 22:34:10 +0530

    Commit 6: Add gravity and vertical velocity logic to Player

--- player.py
+++ player.py
@@ -11,8 +11,22 @@ class Player:
         self.color = BLUE
         
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        
+        # Physics
+        self.vel_y = 0
+        self.gravity = 0.6
+        self.ground_y = HEIGHT - self.height - 20
 
     def update(self):
+        # Apply gravity
+        self.vel_y += self.gravity
+        self.y += self.vel_y
+        
+        # Ground collision
+        if self.y >= self.ground_y:
+            self.y = self.ground_y
+            self.vel_y = 0
+
         # Update rect position based on x and y
         self.rect.x = self.x
         self.rect.y = self.y
