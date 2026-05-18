7cd71826d5adfc7bdc5f8e4ed4ae77cd4b811908
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-05-19 00:16:15 +0530

    Refactor Player class to support sprite images and animations

--- player.py
+++ player.py
@@ -21,6 +21,20 @@ class Player:
         self.vel_y = 0
         self.gravity = 0.6
         self.is_ducking = False
+        
+        # Load sprite images
+        self.images_run = [
+            pygame.image.load("assets/player_run1.png").convert_alpha(),
+            pygame.image.load("assets/player_run2.png").convert_alpha()
+        ]
+        self.image_jump = pygame.image.load("assets/player_jump.png").convert_alpha()
+        self.images_duck = [
+            pygame.image.load("assets/player_duck1.png").convert_alpha(),
+            pygame.image.load("assets/player_duck2.png").convert_alpha()
+        ]
+        
+        self.animation_index = 0.0
+        self.image = self.images_run[0]
 
     @property
     def ground_y(self):
@@ -41,6 +55,17 @@ class Player:
         self.rect.y = self.y
         self.rect.width = self.width
         self.rect.height = self.height
+        
+        # Increment animation index
+        self.animation_index += 0.15
+        
+        # Determine which image to show based on state
+        if self.y < self.ground_y:
+            self.image = self.image_jump
+        elif self.is_ducking:
+            self.image = self.images_duck[int(self.animation_index) % len(self.images_duck)]
+        else:
+            self.image = self.images_run[int(self.animation_index) % len(self.images_run)]
 
     def jump(self):
         # Only jump if on the ground and not ducking
@@ -63,4 +88,5 @@ class Player:
             self.y -= (self.normal_height - self.duck_height)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
+        screen.blit(self.image, self.rect)
+
